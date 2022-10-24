272eb31033610f08983628f8a10932ff496eb69a
Dawana Baker <krunk_011@hotmail.com>
2022-10-24 11:12:30 -0500

    adding products to Products component

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Link from "next/link";
+import { urlFor } from '../lib/client';
+
 
 const FooterBanner = () => {
   return (
-    <div>FooterBanner</div>
+		<div>FooterBanner</div>
   )
 }
 
