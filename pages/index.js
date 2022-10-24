b38cc19e14327af053601f65354ea0b47a1991af
Dawana Baker <krunk_011@hotmail.com>
2022-10-24 10:51:01 -0500

    link sanity data to web app

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,10 @@ import React from 'react'
 import { client } from "../lib/client";
 import { Product, FooterBanner, HeroBanner } from '../components'
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
 
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
@@ -14,7 +14,7 @@ const Home = () => {
       </div>
 
       <div className='products-container'>
-      {  ["Product1", " Product 2"].map((product) => product)}
+      {products?.map((product) => product.name)}
       </div>
 
       <FooterBanner />
@@ -22,4 +22,16 @@ const Home = () => {
   )
 }
 
+export const getServerSideProps = async () => {
+  const query = "*[_type == 'product']";
+  const products = await client.fetch(query);
+
+  const bannerQuery = "*[_type == 'banner']";
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
+
+}
 export default Home
